2ddf385137d30c22b3c5dc53d1c81ea8b7ab704e
Jose De Las Salas <SueBuSo@Joses-MacBook-Pro.local>
2018-06-04 19:24:23 -0400

    added gem image files, attempted .onclick functions, still needs revising, some css styling to resize images of gems evenly.

---
+++ assets/javascript/game.js
@@ -0,0 +1,5 @@
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+$("#stones").on("click", ".time-image", function() {
+    alert("You clicked on Thanos' finger.")
+})
