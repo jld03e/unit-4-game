4d993d11d41465ea6b191e1cb753e33c2ec993bd
Jose De Las Salas <SueBuSo@Joses-MBP.attlocal.net>
2018-06-06 07:06:24 -0400

    finalized the css for the text on the page, still having trouble running function between js page and html page, slacked it to colleague for further assistance.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,4 @@
-<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-
 $("#stones").on("click", ".time-image", function() {
-    alert("You clicked on Thanos' finger.")
-})
+    alert("You clicked on Thanos' finger.");
+
+});
