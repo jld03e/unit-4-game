cb57317c1158b4876a924bad0cce691ec5ec5fbb
Jose De Las Salas <SueBuSo@Joses-MacBook-Pro.local>
2018-06-08 10:15:41 -0400

    submitting game as is, still requires tweaking with reset function.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,81 @@
-$("#stones").on("click", ".time-image", function() {
-    alert("You clicked on Thanos' finger.");
+$(document).ready(function() {
+
+var thanosNumber = Math.floor(Math.random()*((120-19)+1)+19);
+var timeGem = Math.floor(Math.random()* ((12-1)+1)+1);
+var spaceGem = Math.floor(Math.random()* ((12-1)+1)+1);
+var mindGem = Math.floor(Math.random()* ((12-1)+1)+1);
+var soulGem = Math.floor(Math.random()* ((12-1)+1)+1);
+
+var humanNumber = 0
+var winsCounter = 0
+var deathsCounter = 0
+
+$("#wins").text(winsCounter);
+$("#deaths").text(deathsCounter);
+$("#thanosNumber").text(thanosNumber);
+
+function reset() {
+    var thanosNumber = Math.floor(Math.random()*((120-19)+1)+19);
+    $("#thanosNumber").text(thanosNumber);
+    var timeGem = Math.floor(Math.random()* ((12-1)+1)+1);
+    var spaceGem = Math.floor(Math.random()* ((12-1)+1)+1);
+    var mindGem = Math.floor(Math.random()* ((12-1)+1)+1);
+    var humanNumber = 0;
+    $("#humanNumber").text(humanNumber);
+}
+
+function wins() {
+    alert("YOU LIVE.");
+    winsCounter++;
+    $("#wins").text(winsCounter);
+    reset();
+}
+
+function deaths() {
+    alert("THANOS PURGATORY FOR YOU.");
+    deathsCounter++;
+    $("#deaths").text(deathsCounter);
+    reset();
+}
+
+$(".timeGem").on('click', function() {
+    humanNumber = humanNumber + timeGem;
+    $("#humanNumber").text(humanNumber)
+    if (humanNumber == thanosNumber) {
+        wins(); reset();
+    } else if (humanNumber > thanosNumber){
+        deaths(); reset();
+    }
+});
+
+$(".spaceGem").on('click', function() {
+    humanNumber = humanNumber + spaceGem;
+    $("#humanNumber").text(humanNumber)
+    if (humanNumber == thanosNumber) {
+        wins(); reset();
+    } else if (humanNumber > thanosNumber){
+        deaths(); reset();
+    };
+});
+
+$(".mindGem").on('click', function() {
+    humanNumber = humanNumber + mindGem;
+    $("#humanNumber").text(humanNumber)
+    if (humanNumber == thanosNumber) {
+        wins(); reset();
+    } else if (humanNumber > thanosNumber){
+        deaths(); reset();
+    };
+});
+
+$(".soulGem").on('click', function() {
+    humanNumber = humanNumber + soulGem;
+    $("#humanNumber").text(humanNumber)
+    if (humanNumber == thanosNumber) {
+        wins(); reset();
+    } else if (humanNumber > thanosNumber){
+        deaths(); reset();
+    };
+});
 
 });
